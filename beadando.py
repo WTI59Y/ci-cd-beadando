04860028ac60f8da61e0057783450b1e4710dc0a
WTI59Y <mano201611@gmail.com>
2022-11-23 08:29:32 +0100

    WIP

--- beadando.py
+++ beadando.py
@@ -1,13 +1,17 @@
+""" Egyszeresen lancol lista adatstrukturaja """
 class LinkedListNode:
-  def __init__(self, value):
-    self.value = value #értéke
-    self.pointer=None #köv mutató, ami megmutatja hogy hol a következő lista elem
+    def __init__(self, value):
+        """ erteke """
+        self.value = value
+        """ köv mutato, ami megmutatja hogy hol a következo lista elem """
+        self.pointer=None 
 
-class LinkedList: #Egyszeresen láncolt lista definiálása
-  def __init__(self):
-    self.pointer=None
-    
-  def insert_at_start(self, value): #Lista elejébe való beszúrás
-    newnode = LinkedListNode(value)
-    newnode.pointer=self.pointer
-    self.pointer=newnode
+class LinkedList:
+    """ Egyszeresen lancolt lista definialasa """
+    def __init__(self):
+        self.pointer=None
+    """ Lista elejebe valo beszuras """
+    def insert_at_start(self, value):
+        newnode = LinkedListNode(value)
+        newnode.pointer=self.pointer
+        self.pointer=newnode
